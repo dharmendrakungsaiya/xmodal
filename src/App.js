5754f465acb8f4be0370ac56c458794f342077d3
Dharmendra Kungsaiya <dharmendra.kungsaiya20@gmail.com>
2024-07-12 15:22:52 +0530

    issue fix WIP

--- src/App.js
+++ src/App.js
@@ -30,10 +30,12 @@ function App() {
 
   return (
     <div className="App">
+      <div className="modal">
         <h1>User Details Modal</h1>
         <button onClick={clickHandler}>Open Form</button>
+        
         {isOpen && (
-          <div className="modal">
+          
           <div className="modal-content" onClick={closeHandler}>
             <form onSubmit={submitHandler}>
               <h2>Fill Details</h2>
@@ -58,10 +60,10 @@ function App() {
               </button>
             </form>
           </div>
-          </div>
+          
         )}
       </div>
-   
+      </div>
   );
 }
 
