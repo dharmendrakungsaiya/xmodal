e3667ffec299bb66f3447e7f9d77d2b80364ae0f
Dharmendra Kungsaiya <dharmendra.kungsaiya20@gmail.com>
2024-07-12 16:01:19 +0530

    issue fix WIP

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import {useState} from "react";
-import "./App.css";
+
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
+
   const clickHandler = () => {
     setIsOpen(true);
   };
 
   const closeHandler = (e) => {
-    console.log(e.target.className);
-    if (e.target.className === "modal-content") setIsOpen(false);
+    if (e.target.className === "modal-wrapper") {
+      setIsOpen(false);
+    }
   };
 
   const submitHandler = (e) => {
@@ -25,23 +27,21 @@ function App() {
       e.target.dob.value = "";
       setIsOpen(false);
     }
-    console.log(e.target.dob.value);
   };
 
   return (
-    
-      <div className="modal">
-        <h1>User Details Modal</h1>
-        <button onClick={clickHandler}>Open Form</button>
-        
-        {isOpen && (
-          
-          <div className="modal-content" onClick={closeHandler}>
+    <div className="modal">
+      <h1>User Details Modal</h1>
+      <button onClick={clickHandler}>Open Form</button>
+
+      {isOpen && (
+        <div className="modal-wrapper" onClick={closeHandler}>
+          <div className="modal-content">
             <form onSubmit={submitHandler}>
               <h2>Fill Details</h2>
               <div className="input-group">
                 <label htmlFor="username">Username: </label>
-                <input type="text" name="username" id="username" required/>
+                <input type="text" name="username" id="username" required />
               </div>
               <div className="input-group">
                 <label htmlFor="email">Email Address:</label>
@@ -60,12 +60,12 @@ function App() {
               </button>
             </form>
           </div>
-          
-        )}
-      </div>
-    
+        </div>
+      )}
+    </div>
   );
 }
+
 
 
 export default App;
